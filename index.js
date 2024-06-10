4a22ea97d74e3a326f5e3bb1ac16c829da1c6d7a
Redfield-13 <fieldr62@gmail.com>
2024-06-10 14:10:02 +0200

    api final

--- index.js
+++ index.js
@@ -2,49 +2,14 @@ require("dotenv").config();
 const { IgApiClient } = require('instagram-private-api');
 const { get } = require('request-promise');
 const express = require('express');
+
+
+
 const app = express();
 const port = process.env.PORT || 6776;
 
-console.log(process.env.IG_PASSWORD);
 
 const ig = new IgApiClient();
-
-
-ig.state.generateDevice(process.env.IG_USERNAME);
-
-// const postToInsta = async () => {
-    
-//     ig.state.generateDevice(process.env.IG_USERNAME);
-//     const loggedInUser = await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
-    
-//     const imageBuffer = await get({
-//         url: 'https://ai.system.sl/images/3ec3752b1e5e4e07a98e702ba1782708.jpeg',
-//         encoding: null, 
-//     });
-
-//     try{
-//         const res = await ig.publish.photo({
-//             file: imageBuffer,
-//             caption: '4K Frog!!', 
-//         });
-//         console.log(res.media.code);
-//         return({"Post Url :" : "https://www.instagram.com/p/"+res.media.code})
-//     }catch(error){
-//         console.error('Error : ', error);
-//         throw error;
-//     }
-
-    
-    
-// }
-app.use(express.json())
-
-const postToInsta = async (username, password, caption, imageUrl) => {
-    
-    
-
-
-}
 
 app.post('/post', async(req,res) =>{
     console.log(req.body);
